8295c29c7916409d25be8f8aa3265784478787dd
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2024-05-29 12:18:33 -0500

    changes home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,43 @@
-import React from 'react';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import React from 'react';
 
 const Home = () => {
+    const handleCreateClick = () => {
+        window.location.href = 'http://localhost:5173/src/komi/index.html';
+    };
+
+    const handleMoreInfoClick = () => {
+        window.location.href = 'http://localhost:5173/about';
+    };
+
     return (
-        <div className="flex flex-row items-center justify-center mt-10 bg-white px-4 py-16">
-            <div className="text-center mr-16">
-                <h1 className="text-4xl font-semibold text-gray-700 mb-4">
-                    PYTHON <span className="text-black">VISUAL</span>: APRENDE Y DOMINA
-                </h1>
-                <p className="text-xl text-gray-500 mb-8">
-                    TU ASISTENTE INTELIGENTE PARA LA PROGRAMACIÓN VISUAL EN PYTHON
-                </p>
-                <div className="flex justify-center space-x-4">
-                    <button className="flex items-center bg-purple-principal text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition duration-300 hover:bg-purple-700">
-                        <PlayArrowIcon className="mr-2" />
-                        Crear
-                    </button>
-                    <button className="bg-white text-gray-700 border border-gray-300 font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition duration-300 hover:bg-gray-100">
-                        More info
-                    </button>
+        <div className="w-full bg-gradient-to-r from-[#F4E8FF] via-[#F7EEFF] to-[#F4E8FF] flex justify-center items-center" style={{ minHeight: 'calc(100vh - 72px)' }}>
+            <div className="w-full max-w-6xl mx-auto flex flex-col lg:flex-row items-center justify-center p-8">
+                <div className="text-center lg:text-left lg:mr-8 lg:ml-16">
+                    <h1 className="text-4xl font-semibold text-gray-700 mb-2">
+                        PYTHON <span className="text-black">VISUAL</span>: APRENDE Y DOMINA
+                    </h1>
+                    <p className="text-lg text-gray-500 mb-4">
+                        TU ASISTENTE INTELIGENTE PARA LA PROGRAMACIÓN VISUAL EN PYTHON
+                    </p>
+                    <div className="flex justify-center lg:justify-start space-x-2">
+                        <button
+                            onClick={handleCreateClick}
+                            className="flex items-center bg-purple-principal text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition duration-300 hover:bg-purple-700"
+                        >
+                            <PlayArrowIcon className="mr-1" />
+                            Crear
+                        </button>
+                        <button
+                            onClick={handleMoreInfoClick}
+                            className="bg-white text-gray-700 border border-gray-300 font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition duration-300 hover:bg-gray-100"
+                        >
+                            More info
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div className="mt-16">
-                <div className="w-96 h-64 bg-gray-100 flex items-center justify-center">
-                    <img src="/path/to/placeholder-image.png" alt="Placeholder" className="w-full h-full object-cover" />
+                <div className="w-112 h-84 bg-gray-100 flex items-center justify-center rounded-lg">
+                    <img src="src/assets/Demo.gif" alt="Demo GIF" className="w-full h-full object-cover rounded-lg" />
                 </div>
             </div>
         </div>
