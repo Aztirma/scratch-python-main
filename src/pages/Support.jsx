95187994df303835316cb8371c23b77b8b74a5c6
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2024-06-25 16:00:31 -0500

    zzz

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,22 +1,17 @@
+// src/pages/Suggestions.jsx
 import React from 'react';
-import logo from '../assets/logo.png'; // Asegúrate de que la ruta sea correcta
+import FeedbackForm from '../components/FeedbackForm';
+
 
 const Support = () => {
     return (
-        <div className="w-full h-screen flex flex-col justify-center items-center bg-purple-100">
-            <div className="w-full max-w-4xl p-8 bg-white rounded-lg shadow-lg" style={{ minHeight: '90vh' }}>
-                <div className="flex items-center mb-4">
-                    <img src={logo} alt="Logo" className="h-12 mr-4" />
-                    <h1 className="text-2xl font-bold text-purple-700">Asistente virtual</h1>
-                </div>
-                <iframe 
-                    src="https://creator.voiceflow.com/prototype/664fbc81e8061fd0e82cd75e"
-                    className="w-full h-[80vh] rounded-lg border-4 border-purple-500"
-                    title="Support Chatbot"
-                />
+        <div className="flex">
+            <div className="p-6 bg-gray-100 min-h-screen flex-grow">
+                <h1 className="text-3xl font-bold text-purple-600 mb-4">Sugerencias</h1>
+                <FeedbackForm />
             </div>
         </div>
     );
-}
+};
 
 export default Support;
